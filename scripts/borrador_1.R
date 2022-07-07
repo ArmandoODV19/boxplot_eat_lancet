b4af0ba2362c169d2771149f253a54fb29b151cc
armandios19 <fooseaov@gmail.com>
2022-07-07 14:41:13 -0500

    borrador

--- scripts/borrador_1.R
+++ scripts/borrador_1.R
@@ -1,6 +1,14 @@
 # generando los grupos
 
+categoria_de_alimentos <- levels(as.factor(ensanut_100k$categoria))
+categoria_de_alimentos <- as.data.frame(categoria_de_alimentos)
 
+detalles_alimento <- levels(as.factor(ensanut_100k$numero_alimento))
+detalles_alimento <- as.data.frame(detalles_alimento)
 
 carnes <- ensanut_100k %>%
-  filter(categoria %in% c(""))
+  filter(numero_alimento %in% c("carnes_res", "carnes_res_seca", "carne_puerco",
+                                "mariscos", "pescado_fresco", "pescado_seco", "pollo_a",
+                                "pollo_b", "pollo_c", "atun_sardina",
+                                "queso_panela_fresco_cottage", "queso_maduro"))
+

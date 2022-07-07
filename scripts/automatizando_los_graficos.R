0e6145a4b2e7ff7f55773e91acfb5e505988f9c6
armandios19 <fooseaov@gmail.com>
2022-07-07 16:57:41 -0500

    se automatizaron las funciones

--- scripts/automatizando_los_graficos.R
+++ scripts/automatizando_los_graficos.R
@@ -1,20 +1,46 @@
 # automatizando la funcion
 
-eat_general_plot <- function(){
+# boxplot a nivel republica
 
+eat_general_plot <- function(x){
+  x %>%
+    select(numero_alimento, dias_comio) %>%
+    group_by(numero_alimento, dias_comio) %>%
+    summarise(n = n()) %>%
+    mutate(freq = n / sum(n)) %>%
+    filter(dias_comio != 0) %>%
+    ggplot(aes(x = numero_alimento, y = freq, fill = numero_alimento)) +
+    geom_boxplot() +
+    geom_jitter(aes(color = dias_comio), size = 5)+
+    xlab("alimento")+
+    ylab("consumo")+
+    theme_classic()+
+    theme(axis.text.x=element_text(angle = 45, hjust = 1))+
+    guides(fill = FALSE)
 }
 
-carnes %>%
-  select(numero_alimento, dias_comio) %>%
-  group_by(numero_alimento, dias_comio) %>%
-  summarise(n = n()) %>%
-  mutate(freq = n / sum(n)) %>%
-  filter(dias_comio != 0) %>%
-  ggplot(aes(x = numero_alimento, y = freq, fill = numero_alimento)) +
-  geom_boxplot() +
-  geom_jitter(aes(color = dias_comio), size = 5)+
-  xlab("alimento")+
-  ylab("consumo")+
-  theme_classic()+
-  theme(axis.text.x=element_text(angle = 45, hjust = 1))+
-  guides(fill = FALSE)
+eat_general_plot(x = frutas)
+
+
+# boxplot por zona (norte, sur, centro)
+
+eat_zone_plot <- function(x, zone){
+  x %>%
+    filter(region_nutricion == zone) %>%
+    select(numero_alimento, dias_comio) %>%
+    group_by(numero_alimento, dias_comio) %>%
+    summarise(n = n()) %>%
+    mutate(freq = n / sum(n)) %>%
+    filter(dias_comio != 0) %>%
+    ggplot(aes(x = numero_alimento, y = freq, fill = numero_alimento)) +
+    geom_boxplot() +
+    geom_jitter(aes(color = dias_comio), size = 5)+
+    xlab("alimento")+
+    ylab("consumo")+
+    theme_classic()+
+    theme(axis.text.x=element_text(angle = 45, hjust = 1))+
+    guides(fill = FALSE)
+}
+
+eat_zone_plot(x = frutas, zone = "norte")
+

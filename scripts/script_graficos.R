9b813023bbf99da9b34cd978103523e222f68d37
armandios19 <fooseaov@gmail.com>
2022-07-07 16:27:39 -0500

    script graficos

--- scripts/script_graficos.R
+++ scripts/script_graficos.R
@@ -69,10 +69,41 @@ carnes %>%
 carnes %>%
   select(numero_alimento, dias_comio) %>%
   group_by(numero_alimento, dias_comio) %>%
-  summari(n = n()) %>%
-  mutate(freq = n / sum(n))
-
-mtcars %>%
-  group_by(am, gear) %>%
   summarise(n = n()) %>%
   mutate(freq = n / sum(n))
+
+
+### tercer intento
+# este corre y es lo que necesitamos
+
+carnes %>%
+  select(numero_alimento, dias_comio) %>%
+  group_by(numero_alimento, dias_comio) %>%
+  summarise(n = n()) %>%
+  mutate(freq = n / sum(n)) %>%
+  ggplot(aes(x = numero_alimento, y = freq, fill = numero_alimento)) +
+  geom_boxplot() +
+  geom_jitter(aes(color = dias_comio), size = 5)+
+  xlab("alimento")+
+  ylab("consumo")+
+  theme_classic()+
+  theme(axis.text.x=element_text(angle = 45, hjust = 1))+
+  guides(fill = FALSE)
+
+# quitando los ceros
+
+carnes %>%
+  select(numero_alimento, dias_comio) %>%
+  group_by(numero_alimento, dias_comio) %>%
+  summarise(n = n()) %>%
+  mutate(freq = n / sum(n)) %>%
+  filter(dias_comio != 0) %>%
+  ggplot(aes(x = numero_alimento, y = freq, fill = numero_alimento)) +
+  geom_boxplot() +
+  geom_jitter(aes(color = dias_comio), size = 5)+
+  xlab("alimento")+
+  ylab("consumo")+
+  theme_classic()+
+  theme(axis.text.x=element_text(angle = 45, hjust = 1))+
+  guides(fill = FALSE)
+
